f56ce42b2f952c151576653feba7204762f14681
Alfonso Acosta <alfonso.acosta@nekohealth.com>
2023-07-25 01:00:09 +0200

    Add -a (arch) commandline flag

--- src/fat32.h
+++ src/fat32.h
@@ -3,5 +3,5 @@
 #include <stdint.h>
 #include <stddef.h>
 
-void fat32_make_efi(uint32_t image_fd, uint32_t first_sector, uint32_t num_sectors,
-                    const void *kernel, size_t kernel_size);
+void fat32_make_efi(const char *arch, uint32_t image_fd, uint32_t first_sector,
+                    uint32_t num_sectors, const void *kernel, size_t kernel_size);
